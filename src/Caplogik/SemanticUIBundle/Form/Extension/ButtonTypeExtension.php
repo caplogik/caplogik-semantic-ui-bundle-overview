e0cc55d02e88a9d43f1ef88b697aad941c65bf0d
wryk <wryk@wryk.moe>
2017-09-08 20:13:08 +0200

    lower php requirement

--- src/Caplogik/SemanticUIBundle/Form/Extension/ButtonTypeExtension.php
+++ src/Caplogik/SemanticUIBundle/Form/Extension/ButtonTypeExtension.php
@@ -3,12 +3,11 @@
 namespace Caplogik\SemanticUIBundle\Form\Extension;
 
 use Caplogik\SemanticUIBundle\Form\Extension\AbstractTypeExtension;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 
 class ButtonTypeExtension extends AbstractTypeExtension
 {
     public function getExtendedType()
     {
-        return ButtonType::class;
+        return 'Symfony\Component\Form\Extension\Core\Type\ButtonType';
     }
 }
--- src/Caplogik/SemanticUIBundle/Form/Extension/FormTypeExtension.php
+++ src/Caplogik/SemanticUIBundle/Form/Extension/FormTypeExtension.php
@@ -3,12 +3,11 @@
 namespace Caplogik\SemanticUIBundle\Form\Extension;
 
 use Caplogik\SemanticUIBundle\Form\Extension\AbstractTypeExtension;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 
 class FormTypeExtension extends AbstractTypeExtension
 {
     public function getExtendedType()
     {
-        return FormType::class;
+        return 'Symfony\Component\Form\Extension\Core\Type\FormType';
     }
 }
